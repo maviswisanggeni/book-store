1a51e7cc124a76cc2a7bc5f27e52a21c287cd9a6
Mavis Daffa Wisanggeni <mavisdaffawisanggeni22@gmail.com>
2022-11-22 11:14:56 +0700

    add: [MW] display Book data from API in Book List Page

--- lib/views/book_list_page.dart
+++ lib/views/book_list_page.dart
@@ -1,3 +1,6 @@
+import 'dart:convert';
+
+import 'package:book_store/Models/book_list_response.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
@@ -9,12 +12,23 @@ class BookListPage extends StatefulWidget {
 }
 
 class _BookListPageState extends State<BookListPage> {
+  BookListResponse? bookList;
+  bool isLoading = true;
   fetchBookApi() async {
     var url = Uri.parse('https://api.itbook.store/1.0/new');
-    var response =
-        await http.get(url);
+    var response = await http.get(url);
     print('Response status: ${response.statusCode}');
     print('Response body: ${response.body}');
+
+    if (response.statusCode == 200) {
+      final jsonBookList = jsonDecode(response.body);
+      bookList = BookListResponse.fromJson(jsonBookList);
+      Future.delayed(const Duration(seconds: 2), () {
+        setState(() {
+          isLoading = false;
+        });
+      });
+    }
 
     // print(await http.read(Uri.https('example.com', 'foobar.txt')));
   }
@@ -30,9 +44,74 @@ class _BookListPageState extends State<BookListPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Book Catalogue'),
+        elevation: 0,
+        toolbarHeight: 80,
+        title: const Center(child: Text('Book Catalogue')),
       ),
-      body: Container(),
+      body: Container(
+        child: isLoading
+            ? const Center(
+                child: CircularProgressIndicator(),
+              )
+            : ListView.builder(
+                itemCount: bookList!.books!.length,
+                itemBuilder: (context, index) {
+                  final currentBook = bookList!.books![index];
+                  return Container(
+                    margin: const EdgeInsets.symmetric(
+                      horizontal: 20, vertical: 10
+                    ),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: Colors.grey[200],
+                    ),
+                    child: Row(
+                      children: [
+                        Image.network(
+                          currentBook.image!,
+                          width: 110,
+                          height: 110,
+                        ),
+                        Expanded(
+                          flex: 2,
+                          child: Padding(
+                            padding: const EdgeInsets.symmetric(
+                              vertical: 10,
+                            ),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  currentBook.title!,
+                                  style: const TextStyle(
+                                      fontSize: 18, fontWeight: FontWeight.bold,
+                                  overflow: TextOverflow.ellipsis,
+                                  ),
+                                  maxLines: 3,
+                                ),
+                                Text(
+                                  currentBook.price!,
+                                  style: const TextStyle(
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                        Expanded(
+                          child: IconButton(
+                            onPressed: () {},
+                            icon: const Icon(Icons.keyboard_arrow_right_rounded),
+                          ),
+                        ),
+                      ],
+                    ),
+                  );
+                },
+              ),
+      ),
     );
   }
 }
