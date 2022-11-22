166a7ae997fd34d6af5b709eaa5a8de2a9e911ef
Mavis Daffa Wisanggeni <mavisdaffawisanggeni22@gmail.com>
2022-11-22 09:49:18 +0700

    fetch: [MW] fetch Book API

---
+++ lib/views/book_list_page.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class BookListPage extends StatefulWidget {
+  const BookListPage({Key? key}) : super(key: key);
+
+  @override
+  State<BookListPage> createState() => _BookListPageState();
+}
+
+class _BookListPageState extends State<BookListPage> {
+  fetchBookApi() async {
+    var url = Uri.parse('https://api.itbook.store/1.0/new');
+    var response =
+        await http.post(url, body: {'name': 'doodle', 'color': 'blue'});
+    print('Response status: ${response.statusCode}');
+    print('Response body: ${response.body}');
+
+    // print(await http.read(Uri.https('example.com', 'foobar.txt')));
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    fetchBookApi();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Book Catalogue'),
+      ),
+      body: Container(),
+    );
+  }
+}
